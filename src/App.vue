3a559e686c92acaa884839a45c94283e85dd492f
max6555 <khanhtoan.le.6555@gmail.com>
2022-12-19 00:46:11 +0700

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <main-sidebar></main-sidebar>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainSidebar from './components/layout/MainSidebar.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainSidebar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scope>
+@import url(./assets/css/resetCss.css);
+@import url(./assets/css/base.css);
+  .container {
+  overflow-y: hidden;
+  display: flex;
+  }
+  
 </style>
---
+++ src/components/layout/MainSidebar.vue
@@ -0,0 +1,347 @@
+<template>
+  <div class="sidebar">
+            <div id="menu-btn" class="header__left-side-menu-container">
+                <div class="header-left-side__menu-button">
+                </div>
+            </div>
+            <!-- <div class="sidebar__header-wrap"> -->
+            <div class="sidebar__header">
+                <div class="sidebar__header--icon">
+                </div>
+                <div class="sidebar__header--logo">
+                    <img data-v-e206d9c0=""
+                        src="https://actappg1.misacdn.net/img/Logo_Module_TiengViet_White.66947422.svg"
+                        class="sidebar__header--logo-image">
+                </div>
+            </div>
+            <!-- </div> -->
+
+            <div class="sidebar__menu">
+                <div class="sidebar__menu-list">
+                    <div class="sidebar__menu--item sidebar__menu-item-selected">
+                        <div class="sidebar__item--icon">
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Tổng quát
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Tổng quát</div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-73px -1628px;">
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Tiền mặt
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Tiền mặt
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-117px -1628px;">
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Tiền gửi
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Tiền gửi
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-161px -1628px;">
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Mua hàng
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Mua hàng
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-204px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Bán Hàng
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Bán Hàng
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-249px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Quản lý đơn hàng
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Quản lý đơn hàng
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-291px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Kho
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Kho
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-336px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Công cụ dụng cụ
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Công cụ dụng cụ
+                        </div>
+
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-378px -1630px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Tài sản cố định
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Tài sản cố định
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-467px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Tiền lương
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Tiền lương
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-423px -1630px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Thuế
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Thuế
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-467px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Giá Thành
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Giá Thành
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-378px -1659px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Ngân sách
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Ngân sách
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-547px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Báo cáo
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Báo cáo
+                        </div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-204px -1664px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Phân tích tài chính
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Phân tích tài chính</div>
+                    </div>
+                    <div class="sidebar__menu--item">
+                        <div class="sidebar__item--icon" style="background-position:-467px -1628px">
+
+                        </div>
+                        <div class="sidebar__item--label">
+                            <span class="sidebar__item--label-text">
+                                Giá Thành
+                            </span>
+                        </div>
+                        <div class="sidebar__item-tool-tip display_none">
+                            Giá Thành
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: "MainSideBar",
+}
+</script>
+
+<style scoped>
+.sidebar {
+  background-color: #393a3d;
+  width: 200px;
+  /* min-height: 100vh; */
+  height: 100vh;
+  /* max-height: 100vh; */
+  /* overflow: auto; */
+  position: relative;
+  /* overflow-y: auto;
+	overflow-x:hidden; */
+}
+.sidebar__header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 0;
+  background-color: #393a3d;
+  /* position:sticky;
+	top:0; */
+}
+.sidebar__header--icon {
+  width: 24px;
+  height: 24px;
+  background: url(../../assets/images/icon.svg) no-repeat -424px -86px;
+  margin-right: 10px;
+}
+.sidebar__header--logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.sidebar__header--logo-image {
+  height: 32px;
+}
+/* --sidebar menu */
+.sidebar__menu {
+  padding-top: 24px;
+  overflow-y: visible;
+  height: calc(100vh - 80px);
+  overflow-x: visible;
+}
+.sidebar__menu-list{
+  
+/* min-height: 100vh; */
+  
+/* height: 100%; */
+  
+/* overflow-y: auto; */
+}
+.sidebar__menu--item { 
+  height: 42px;
+  display: flex;
+  align-items: center;
+  padding: 0 14px;
+  cursor: pointer;
+  position: relative;
+}
+.sidebar__menu-item-selected{
+  box-sizing: border-box;
+  padding-left:calc(14px - 6px) !important;
+  border-left:6px solid #2ca01c;
+}
+.sidebar__menu-item-selected .sidebar__item--label-text{
+	font-weight: 600;
+}
+
+.sidebar__item--icon {
+  width: 24px;
+  height: 24px;
+  background: url(../../assets/images/icon.svg) no-repeat -30px -1628px;
+  margin-right: 8px;
+}
+.sidebar__item--label {
+}
+.sidebar__item--label-text {
+	user-select: none;
+  color: white;
+}
+.sidebar__item-tool-tip{
+  position: absolute;
+  left: calc(100% + 8px) ;
+  /* width:94px; */
+  padding:0 12px;
+  height: 40px;
+  background-color:var(--sidebar-color) ;
+  top: 50%;
+  transform: translateY(-50%);
+  bottom:0;
+  white-space: nowrap;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  opacity: 94%;
+  color:#FFFFFF;
+  text-align: center;
+  z-index: 10001;
+}
+.sidebar__item-tool-tip::before{
+  content: ""; 
+  background-color:var(--sidebar-color);
+  width:7px;
+  height:7px;
+  right:calc(100% - 3.5px);
+  transform: rotate(45deg);
+  position: absolute;
+}
+
+.sidebar__menu--item:hover {
+  background-color: #58595d;
+}
+</style>
